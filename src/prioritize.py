0f51c336396f483b224a64392ee81d8d02a64a4b
Romain Arnaud <rarnaud@redhat.com>
2023-03-24 13:46:46 -0400

    Add main function
    Signed-off-by: Romain Arnaud <rarnaud@redhat.com>

--- src/prioritize.py
+++ src/prioritize.py
@@ -37,52 +37,56 @@ def get_args():
     return parser.parse_args()
 
 
-args = get_args()
+def main():
+    args = get_args()
 
-url = os.environ.get("JIRA_URL", "https://issues.redhat.com")
-token = os.environ.get("JIRA_TOKEN")
-if not token:
-    print("Set JIRA_TOKEN environment variable to your JIRA personal access token")
-    sys.exit(1)
+    url = os.environ.get("JIRA_URL", "https://issues.redhat.com")
+    token = os.environ.get("JIRA_TOKEN")
+    if not token:
+        print("Set JIRA_TOKEN environment variable to your JIRA personal access token")
+        sys.exit(1)
 
-JIRA = jira.client.JIRA(server=url, token_auth=token)
+    JIRA = jira.client.JIRA(server=url, token_auth=token)
 
-query = f"key={args.feature} and type=Feature"
-print("Confirming the Feature exists:")
-print("  > " + query)
-results = JIRA.search_issues(query)
-if not results:
-    print(f"Feature not found via query: {query}")
-    sys.exit(1)
-feature = results[0]
+    query = f"key={args.feature} and type=Feature"
+    print("Confirming the Feature exists:")
+    print("  > " + query)
+    results = JIRA.search_issues(query)
+    if not results:
+        print(f"Feature not found via query: {query}")
+        sys.exit(1)
+    feature = results[0]
 
-query = f'issueFunction in portfolioChildrenOf("key={feature}")'
-print("Looking up epics on the feature")
-print("  > " + query)
-epics = JIRA.search_issues(query)
+    query = f'issueFunction in portfolioChildrenOf("key={feature}")'
+    print("Looking up epics on the feature")
+    print("  > " + query)
+    epics = JIRA.search_issues(query)
 
-if not epics:
-    print("No epics found.")
-    sys.exit(1)
+    if not epics:
+        print("No epics found.")
+        sys.exit(1)
 
-epic_keys = ",".join([epic.key for epic in epics])
-query = f'issueFunction in issuesInEpics("key in ({epic_keys})") and project="{args.project}" and statusCategory != Done'
-print(f"Looking up stories in {args.project} on those epics")
-print("  > " + query)
-stories = JIRA.search_issues(query)
+    epic_keys = ",".join([epic.key for epic in epics])
+    query = f'issueFunction in issuesInEpics("key in ({epic_keys})") and project="{args.project}" and statusCategory != Done'
+    print(f"Looking up stories in {args.project} on those epics")
+    print("  > " + query)
+    stories = JIRA.search_issues(query)
 
-query = f'project="{args.project}" ORDER BY Rank Asc'
-print("Finding current highest ranked story in the project")
-print("  > " + query)
-current_order = JIRA.search_issues(query)
-highest = current_order[0]
+    query = f'project="{args.project}" ORDER BY Rank Asc'
+    print("Finding current highest ranked story in the project")
+    print("  > " + query)
+    current_order = JIRA.search_issues(query)
+    highest = current_order[0]
 
-print()
-print(f"Moving {len(stories)} stories higher than {highest.key}")
-for story in stories:
-    if story == highest:
-        print(f"  Ignoring {story.key}. It is already ranked highest.")
-        continue
-    print(f"  Moving rank of {url}/browse/{story.key} above {highest.key}")
-    JIRA.rank(issue=story.key, prev_issue=highest.key)
-print("Done.")
+    print()
+    print(f"Moving {len(stories)} stories higher than {highest.key}")
+    for story in stories:
+        if story == highest:
+            print(f"  Ignoring {story.key}. It is already ranked highest.")
+            continue
+        print(f"  Moving rank of {url}/browse/{story.key} above {highest.key}")
+        JIRA.rank(issue=story.key, prev_issue=highest.key)
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
