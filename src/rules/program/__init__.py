981118019449fbabda286d8e75a8fc5c71c130d8
Ralph Bean <rbean@redhat.com>
2023-12-21 16:16:06 -0500

    Add component propagation from features to outcomes
    This is useful for seeing at a glance which teams contribute to which
    outcomes.

--- src/rules/program/__init__.py
+++ src/rules/program/__init__.py
@@ -1,1 +1,2 @@
+from .components import check_components
 from .target_end import check_target_end_date
---
+++ src/rules/program/components.py
@@ -0,0 +1,35 @@
+from time import strftime
+
+import jira
+
+from utils.jira import refresh, update
+
+
+def check_components(issue: jira.resources.Issue, context: dict, dry_run: bool) -> None:
+    children = issue.raw["Context"]["Related Issues"]["Children"]
+
+    child_components = list(set(sum([i.fields.components for i in children], [])))
+    child_components = [c.name for c in child_components]
+    child_components.sort()
+
+    components = issue.fields.components
+    components = [c.name for c in components]
+    components.sort()
+
+    to_add = set(child_components) - set(components)
+    to_remove = set(components) - set(child_components)
+
+    if to_add:
+        context["updates"].append(f"  > Adding components {to_add}.")
+    if to_remove:
+        context["updates"].append(f"  > Removing components {to_remove}.")
+
+    if not dry_run and (to_add or to_remove):
+        update(
+            issue,
+            data=dict(
+                update={
+                    "components": [{"set": [{"name": c} for c in child_components]}]
+                }
+            ),
+        )
